a9c905ea8b644c56c8d7914df4795fa41ca3c161
Lemon Slemon <slemonide@gmail.com>
2015-08-28 00:59:10 -0700

    Include timer

--- random_eq.py
+++ random_eq.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import random
 import math
+from time import time
 
 numbers = range(1, 10)
 
 count = 1
+t_0 = time()
 while True:
     num1 = random.choice(numbers)
     num2 = random.choice(numbers)
@@ -18,3 +20,8 @@ while True:
 
     if count == len(numbers) + 1:
         break
+
+t = time()
+t_delta = t - t_0
+mind_power = len(numbers) / t_delta * (10 ** 3) #(10 ** 3) represents "milli-"
+print('Your mind power: ' + str(mind_power) + '1/ms')
